81a1bfe72f02bfc3fbe66fbb187857d9f2cdd812
amarik <aandrew.marik@gmal.com>
2016-02-25 19:20:44 +0200

    use router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const koa = require('koa');
+const router = require('koa-router');
 const app = koa();
-
-app.use(function *(){
-	this.body = 'List of companies';
-});
+const publicRouter = new router();
 
 function *reqlogger(next){
   console.log('%s - %s %s',new Date().toISOString(), this.req.method, this.req.url);
   yield next;
 }
 
+app.use(reqlogger);
+
+publicRouter.get('/auth/github',function *(){
+
+});
 
 app.listen(3000, ()=> { 
 	console.log('app is listening on http://localhost:3000/')
